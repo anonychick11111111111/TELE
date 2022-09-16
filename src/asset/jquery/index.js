fab790dfd80d16f8e90c7769e2c4c74e86e59f03
anonychick11111111111 <cuong.dd187593@sis.hust.edu.vn>
2022-09-16 14:35:43 +0700

    fix

--- src/asset/jquery/index.js
+++ src/asset/jquery/index.js
@@ -1,42 +1,24 @@
-$(document).ready(function(){
-    $('.container1 .left_elem').click(function(){
-        $('.container1 .left_elem').removeClass('active');
-        $(this).addClass('active');
-        return false;
-    });
-    
-
+$(document).ready(function(){   
     $('#button_right').click(function() {
         var classActive = $('.container1 .active');
         classActive.hide();
         $('#button_right').css("background-color", "yellow");
         $('#button_left').removeAttr("style");
-        $('.container2 .left_elem').click(function(){
-            $('.container2 .left_elem').removeClass('active');
-            $(this).addClass('active');
-            return false;
-        });
         $('.container2').append(classActive);
         classActive.removeClass('active');
         classActive.removeAttr("style");
     });
 
-    $('.container2 .left_elem').click(function(){
-        $('.container2 .left_elem').removeClass('active');
+    $(document).on("click", ".left_elem", function(){
+        $(".left_elem").removeClass('active');
         $(this).addClass('active');
-        return false;
-    });
+    })
 
     $('#button_left').click(function() {
         var classActivee = $('.container2 .active');
         $('#button_left').css("background-color", "yellow");
         $('#button_right').removeAttr("style")
-        classActivee.hide();
-        $('.container1 .left_elem').click(function(){
-            $('.container1 .left_elem').removeClass('active');
-            $(this).addClass('active');
-            return false;
-        });       
+        classActivee.hide();     
         $('.container1').append(classActivee);
         classActivee.removeClass('active');
         classActivee.removeAttr("style");
